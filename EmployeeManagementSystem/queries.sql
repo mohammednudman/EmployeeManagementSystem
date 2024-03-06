246bca618406c4d166ba3d44cc334dc474e378c3
Mohammed Nudman Raza Shaikh <mohammednudman@gmail.com>
2024-03-06 16:33:39 +0530

    Project Done

---
+++ EmployeeManagementSystem/queries.sql
@@ -0,0 +1,28 @@
+﻿CREATE TABLE users
+(
+	id INT PRIMARY KEY IDENTITY(1,1),
+	username VARCHAR(MAX) NULL,
+	password VARCHAR(MAX) NULL,
+	date_register DATE NULL
+);
+
+SELECT * FROM users;
+
+CREATE TABLE employees 
+(
+	id INT PRIMARY KEY IDENTITY(1,1),
+	employee_id VARCHAR(MAX) NULL,
+	full_name VARCHAR(MAX) NULL,
+	gender VARCHAR(MAX) NULL,
+	contact_number VARCHAR(MAX) NULL,
+	position VARCHAR(MAX) NULL,
+	image VARCHAR(MAX) NULL,
+	insert_date DATE NULL,
+	update_date DATE NULL,
+	delete_date DATE NULL,
+	salary INT NULL
+)
+
+SELECT * FROM employees;
+
+ALTER TABLE employees ADD status VARCHAR(MAX) NULL
